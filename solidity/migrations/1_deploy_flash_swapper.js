0497e86dd1efe0d77881233672e41816fbb2ee25
Robbe Louwet <robbe.louwet@the47.be>
2021-08-26 09:37:41 +0200

    sc ready for deployment

--- solidity/migrations/1_deploy_flash_swapper.js
+++ solidity/migrations/1_deploy_flash_swapper.js
@@ -1,7 +1,9 @@
 const Arbitrage = artifacts.require("Arbitrage");
 
 module.exports = function (deployer) {
-	// pancakefactory: 0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73
-
-	deployer.deploy(Arbitrage);
+	deployer.deploy(
+		Arbitrage,
+		"0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73", // PancakeFactory address
+		"0xCDe540d7eAFE93aC5fE6233Bee57E1270D3E330F"
+	);
 };
